0d08bf3662324b4c4125b6c391824e2dcaceabce
siam.kiw <siam.kiw@gmail.com>
2021-09-03 09:37:12 +0700

    init project

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require("fs")
+const http = require("http")
+const {WebSocketServer} = require("ws")
+
+const port = 8080
+const wss1 = new WebSocketServer({ noServer: true });
+
+wss1.on('connection', function connection(ws) {
+    // do some thing
+    console.log("on connection")
+
+    ws.on('message', function incoming(data) {
+        wss1.clients.forEach(function each(client) {
+            let str = new TextDecoder().decode(data); 
+            client.send(str);
+        //   if (client !== ws && client.readyState === WebSocket.OPEN) {
+        //     client.send(data);
+        //   }
+        })
+      })
+  });
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/" && req.method === "GET"){
+        console.log("on GET /")
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        const files = fs.readFileSync("./static/index.html");
+
+        res.write(files)
+
+        res.end()
+    }
+})
+
+server.on('upgrade', function upgrade(req, socket, head){
+
+  if (req.url === '/ws') {
+    wss1.handleUpgrade(req, socket, head, function done(ws) {
+      wss1.emit('connection', ws, req);
+    });
+  } else {
+    socket.destroy();
+  }
+})
+
+server.listen(port, () => {
+    console.log("listem on port ", port)
+})
